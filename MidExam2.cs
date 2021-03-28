d62632d1a1c74b733927e07771ca312f67903625
chayada08 <chayada.0802@mail.kmutt.ac.th>
2021-03-28 08:34:57 +0700

    finish File

---
+++ MidExam2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("input number a : ");
+            a = int.Parse(Console.ReadLine());
+            int b;
+            Console.Write("input number b : ");
+            b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+            while(x!=y)
+            {
+                if(x<y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
